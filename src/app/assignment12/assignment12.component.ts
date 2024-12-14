657e3b134cd5f5f0f830e1af685638a862298896
168AM-Saurabh <saurabh.168AM@yahoo.com>
2024-12-14 07:28:29 +0530

    assignment13

--- src/app/assignment12/assignment12.component.ts
+++ src/app/assignment12/assignment12.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {ReactiveFormsModule} from '@angular/forms';
+import {FormBuilder,Validators} from '@angular/forms'
 
 @Component({
   selector: 'app-assignment12',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,ReactiveFormsModule],
   templateUrl: './assignment12.component.html',
   styleUrl: './assignment12.component.css'
 })
 export class Assignment12Component {
+	public fbobj = new FormBuilder();
+//    validUsernamePattern = "[a-zA-Z]";
+    validPhoneePattern = "^[0-9]{10}$";
+    validEmailPattern = "^[a-zA-Z0-9]+@[a-zA-Z]+\.[a-zA-Z]{2,}$";
+    validCitynamePattern = "^[a-zA-Z]{4,}$";
+    validZipCodePattern = "^[0-9]{5}$";
 
+    
+    formDemo = this.fbobj.group({
+        firstname :['', [Validators.required,Validators.pattern('[a-zA-Z]*')] ],
+        lastname :['', [Validators.required,Validators.pattern('[a-zA-Z]*')] ],
+        email :['', [Validators.required, Validators.minLength(3),Validators.pattern(this.validEmailPattern)] ],
+        phone :['', [Validators.required,Validators.pattern(this.validPhoneePattern)] ],
+        city :['', [Validators.required,Validators.pattern(this.validCitynamePattern)] ],
+        state :['', [Validators.pattern(this.validCitynamePattern)] ],
+        zipcode :['', [Validators.pattern(this.validZipCodePattern)] ],
+        comments :['', [Validators.minLength(30)] ]
+    
+    });
+   
 }
